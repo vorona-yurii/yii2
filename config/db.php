73d29a2417cb6ccda2f955f26693e308ca301cd1
Yurii Vorona <voronayu1331@gmail.com>
2018-03-01 00:26:25 +0200

    fix db.php

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     // 'dsn' => env('DB_DSN', 'sqlite:/path/to/database/file'),
-    'dsn' => 'mysql:host=' . env('DB_HOST', 'localhost') . ';dbname=' . env('DB_NAME', 'yii2basic'),
-    'username' => env('DB_USERNAME', 'root'),
-    'password' => env('DB_PASSWORD', ''),
+    'dsn' => 'mysql:host=' . env('DB_HOST', 'localhost') . ';dbname=' . env('DB_NAME'),
+    'username' => env('DB_USERNAME'),
+    'password' => env('DB_PASSWORD'),
     'charset' => 'utf8',
 ];
