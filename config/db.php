b6fef3f88d31440b5d562ba031557be8e6bee1e5
Yurii Vorona <voronayu1331@gmail.com>
2018-02-28 12:14:41 +0200

    fix gitignore | db config

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yuv_yii2',
+    'username' => 'yuv_yii2',
+    'password' => '13vorona1998',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
